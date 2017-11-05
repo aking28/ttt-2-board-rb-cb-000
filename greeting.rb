55c34661ced73a188fc76dec60e6994bdebf4e79
Alexandra King <aking28@me.com>
2017-11-05 17:30:33 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -7,3 +7,7 @@ puts "There are num_of_attendees people coming to Beyonce's birthday party."
 
 answer = "< fill in your answer here >"
 puts "A group of flamingos is called a #{answer}."
+
+def print_name
+  puts "Guy Fieri"
+end  
