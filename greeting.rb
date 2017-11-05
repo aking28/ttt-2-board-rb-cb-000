9cb515e1e5975b064d2544fbb6e2b625fd8b149b
Alexandra King <aking28@me.com>
2017-11-05 17:31:41 +0000

    automatically backed up by learn

--- greeting.rb
+++ greeting.rb
@@ -1,12 +1,3 @@
-def greeting
-  puts "Hellow World"
-end
-greeting
-
-puts "There are num_of_attendees people coming to Beyonce's birthday party."
-
-answer = "< fill in your answer here >"
-puts "A group of flamingos is called a #{answer}."
 
 def print_name
   puts "Guy Fieri"
